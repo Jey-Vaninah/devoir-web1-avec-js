37f160b3ad09100627101b9eb8cc73e2b066fe96
Jey-Vaninah <hei.vaninah@gmail.com>
2024-04-27 17:41:41 +0200

    finalisation

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-const button = document.querySelector(".button")
-button.addEventListener("click", function () {
-    const sujet = document.querySelector("#sujet").value;
-    const reponse = document.querySelector("reponse").value;
-    let score =0;
-    
-    if (sujet === reponse ) {
+const form = document.querySelector("form");
+const phrases = [
+   "hello worlds from my game",
+   "this game is amazing",
+];
+const words = phrases.join(" ").trim().split(" ");
 
-       score = score + 1;
-    }
-    else {
-       score =score
-    }
+updateSujet();
 
-})
+// --------------------------------------------------
+form.addEventListener("submit", (event)=>{
+   event.preventDefault();
+   const sujet = document.querySelector("#sujet").textContent;
+   const reponse = document.querySelector("#reponse").value;
+
+   if(sujet == reponse){
+      const oldValue = document.querySelector("#score").textContent;
+      document.querySelector("#score").textContent = Number(oldValue) + 1;
+   }
+   
+   updateSujet();
+   document.querySelector("#reponse").value = "";
+});
+
+document.querySelector("#phrases-radio").addEventListener("change", (event)=>{
+   updateSujet();
+});
+document.querySelector("#words-radio").addEventListener("change", (event)=>{
+   updateSujet();
+});
+
+function getRandomIndex(a, b){
+   return Math.floor(Math.random() * (b - a)) + a;
+}
+
+function updateSujet(){
+   const isWords = document.querySelector("#words-radio").checked;
+   current = isWords ? words[getRandomIndex(0, words.length)] : phrases[getRandomIndex(0, phrases.length)];
+   document.querySelector("#sujet").textContent = current;
+}
